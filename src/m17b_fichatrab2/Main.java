0be890c9573536a807d7d5d85f20526c518a61ee
a15820 <a15820@d1-08-04.dped.local>
2017-02-08 11:03:44 +0000

    versão beta 0.2.2

--- src/m17b_fichatrab2/Main.java
+++ src/m17b_fichatrab2/Main.java
@@ -1,11 +1,16 @@
 package m17b_fichatrab2;
-
+/**
+ * Class Main  
+ * @author Hugo Duarte
+ * @author Nuno calvinho
+ * @version 2.0
+ */
 public class Main {
 
     public static void main(String[] args) {
-        // Ficha de Trabalho #2
-        Ponto2D p1 = new Ponto2D(5,7);
-        Ponto2D p2 = new Ponto2D(2,8);
+        
+        Ponto2D p1 = new Ponto2D(5,7);          //Coordenadas do primeiro ponto
+        Ponto2D p2 = new Ponto2D(2,8);          //Coordenadas do segundo ponto
         
         System.out.println(p1);
         System.out.println(p2);
--- src/m17b_fichatrab2/Ponto2D.java
+++ src/m17b_fichatrab2/Ponto2D.java
@@ -1,44 +1,66 @@
 package m17b_fichatrab2;
-
+/**
+ * Class Ponto2D 
+ * @author Hugo Duarte
+ * @author Nuno calvinho
+ * @version 2.0
+ */
 public class Ponto2D {
-    private int x;
-    private int y;
+    private int x;                              //atributo x;
+    private int y;                              //atributo y;
     
-    public Ponto2D(){
+    public Ponto2D(){                           //Construtor não parameterizado;
         x=0;
         y=0;        
     } 
-    // Faltam construtor, getters e setters.
     
-    public Ponto2D(int x, int y){
+    public Ponto2D(int x, int y){               // Construtor parameterezido;
         this.x = x;
         this.y = y;        
     }
     
-    
-    public double getX(double x){           //obtem o valor para x;
-        
-        return x;
+    @Override
+    public String toString(){                   //Mostra os valores dos pontos x
+        return"X: "+getX()+" Y: "+getY();                 //e y
     }
     
-    public double getY(double y){           //obtem o valor para y;
-        
+   /**
+     * @return the x
+     */
+    public int getX() {
+        return x;
+    }
+
+    /**
+     * @return the y
+     */
+    public int getY() {
         return y;
     }
     
-    public double distancia(Ponto2D a){
-                                        
-        return x-y;
+    public double distancia(Ponto2D a){         //Função que obtem a distância 
+        double distancia= 0;                    // entre os dois pontos;    
+        
+        distancia = Math.sqrt(Math.pow((getX() - a.getX()),2) + Math.pow((getY() - a.getY()),2 ));
+        
+        return distancia;
+    }
+
+    /**
+     * @param x the x to set
+     */
+    public void setX(int x) {
+        this.x = x;
+    }
+
+    /**
+     * @param y the y to set
+     */
+    public void setY(int y) {
+        this.y = y;
     }
     
-    public void setX(){                     //devolve o valor do X;
-        
-        
-    }
-    
-    public void setY(){                     //devolve o valor do y;    
-        
-       
-    }
+ 
+
     
 }
