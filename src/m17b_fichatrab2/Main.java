cd3d096e71b1f1acd06d8c422f926137b88645a5
a15820 <a15820@d1-08-04.dped.local>
2017-02-08 11:15:49 +0000

    versão Final

--- src/m17b_fichatrab2/Main.java
+++ src/m17b_fichatrab2/Main.java
@@ -6,12 +6,15 @@ package m17b_fichatrab2;
  * @version 2.0
  */
 public class Main {
-
+    /**
+    * 
+    * @param args 
+    */
     public static void main(String[] args) {
         
         Ponto2D p1 = new Ponto2D(5,7);          //Coordenadas do primeiro ponto
         Ponto2D p2 = new Ponto2D(2,8);          //Coordenadas do segundo ponto
-        
+       
         System.out.println(p1);
         System.out.println(p2);
         
